02c863d8b66813c03a55888d91c83beda0e4fb1f
hua_bang <47221942+hua-bang@users.noreply.github.com>
2023-10-12 23:36:38 +0800

    ✨ feat(custom-hook): useLatest

--- src/custom-hooks/useLatest/index.ts
+++ src/custom-hooks/useLatest/index.ts
@@ -1,10 +1,11 @@
 import { useRef, MutableRefObject } from 'react';
 
-const useLatest = <T>(val: T): MutableRefObject<T> => {
-  const ref = useRef<T>(val);
-  ref.current = val;
+function useLatest<T>(value: T): MutableRefObject<T> {
+  const ref = useRef<T>(value);
+
+  ref.current = value;
 
   return ref;
-};
+}
 
 export default useLatest;
