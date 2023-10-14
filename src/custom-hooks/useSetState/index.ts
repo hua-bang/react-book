92a541356516153dcf77ff198c8f3a0ba1f872d8
hua_bang <47221942+hua-bang@users.noreply.github.com>
2023-10-14 15:42:31 +0800

    ✨ feat(custom-hook): useSetState

--- src/custom-hooks/useSetState/index.ts
+++ src/custom-hooks/useSetState/index.ts
@@ -1,15 +1,15 @@
-import { useCallback, useState } from 'react';
-import { isFunction } from '../../utils';
-
-export type SetState<S extends Record<string, any>> = (
-  state: Partial<S> | ((prev: S) => S),
-) => void;
+import { useState } from 'react';
+import { useCallback } from 'react';
+import { isFunction } from '../utils';
 
 const useSetState = <S extends Record<string, any>>(
-  initialVal: S | (() => S),
-): [S, SetState<S>] => {
-  const [state, setState] = useState(initialVal);
-  const setMergeState: SetState<S> = useCallback(patch => {
+  initState: S | (() => S),
+) => {
+  const [state, setState] = useState<S>(initState);
+
+  const setMergeState: React.Dispatch<React.SetStateAction<
+    Partial<S>
+  >> = useCallback(patch => {
     setState(prevState => {
       const newState = isFunction(patch) ? patch(prevState) : patch;
       return newState ? { ...prevState, ...newState } : prevState;
